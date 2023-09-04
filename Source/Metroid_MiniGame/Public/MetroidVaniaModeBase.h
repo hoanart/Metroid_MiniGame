6f062e3d0f962c1149423b5d8564277e95fb9d99
Seongho <hoanart@naver.com>
2023-09-04 15:40:28 +0900

    Add Game Mode

---
+++ Source/Metroid_MiniGame/Public/MetroidVaniaModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MetroidVaniaModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class METROID_MINIGAME_API AMetroidVaniaModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
