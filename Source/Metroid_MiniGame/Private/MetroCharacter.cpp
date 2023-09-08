9ccf6e3a3efb11eb556718bf05906225dd27a3f2
Seongho <hoanart@naver.com>
2023-09-08 17:33:25 +0900

    Add Riffle Animation and Sounds

--- Source/Metroid_MiniGame/Private/MetroCharacter.cpp
+++ Source/Metroid_MiniGame/Private/MetroCharacter.cpp
@@ -3,6 +3,8 @@
 
 #include "MetroCharacter.h"
 
+#include "EnhancedInputComponent.h"
+
 AMetroCharacter::AMetroCharacter()
 {
 	mRiffleGuns.Reserve(3);
@@ -18,4 +20,40 @@ AMetroCharacter::AMetroCharacter()
 void AMetroCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+void AMetroCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if(TObjectPtr<UEnhancedInputComponent> EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(EquipAction,ETriggerEvent::Triggered ,this,&AMetroCharacter::Equip);
+	}
+}
+
+const bool& AMetroCharacter::IsRiffle() const
+{
+	return mbRiffle;
+}
+
+void AMetroCharacter::SetRiffle(const bool& bRiffle)
+{
+	mbRiffle = bRiffle;
+}
+
+void AMetroCharacter::Equip()
+{
+	if(mbEquip)
+	{
+		if(mbRiffle)
+		{
+			mRiffleGuns[0]->SetHiddenInGame(true);
+			mRiffleGuns[1]->SetHiddenInGame(false);
+		}
+		else
+		{
+			mRiffleGuns[0]->SetHiddenInGame(false);
+			mRiffleGuns[1]->SetHiddenInGame(true);
+		}
+	}
 }
--- Source/Metroid_MiniGame/Public/MetroCharacter.h
+++ Source/Metroid_MiniGame/Public/MetroCharacter.h
@@ -17,12 +17,34 @@ public:
 	AMetroCharacter();
 protected:
 	virtual void BeginPlay() override;
-
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 private:
 
 
+	//FUNCTION
+public:
+	UFUNCTION(BlueprintGetter)
+	const bool& IsRiffle() const;
+	UFUNCTION(BlueprintSetter)
+	void SetRiffle(const bool& bRiffle);
 private:
+	UFUNCTION()
+	void Equip();
+	//PROPERTY
+public:
+
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Gun")
+	bool mbEquip;
+	UPROPERTY(EditAnywhere,BlueprintGetter= IsRiffle, BlueprintSetter= SetRiffle, Category = "Gun")
+	bool mbRiffle;
+	
+	//UObject
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> EquipAction;
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly, Instanced,Category="Components",meta = (AllowPrivateAccess = true))
 	TArray<TObjectPtr<UStaticMeshComponent>> mRiffleGuns;
-
+	
 };
